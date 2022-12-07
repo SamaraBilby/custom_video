3a3c9a45550faf9d9f6dd70a873e1b57a061061b
SamaraBilby <samarabilby@hotmail.com>
2022-12-07 17:01:56 -0300

    finalização CSS e inicio do JavaScript

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("video");
+const play= document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Event Listeners
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('click', toggleVideoStatus);
+
